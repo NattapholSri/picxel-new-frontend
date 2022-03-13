f2e6b19dd1cee5df8ba7e6088cbb73cdb7176658
Nattaphol <s6101012630097@email.kmutnb.ac.th>
2022-03-13 21:19:36 +0700

    updating postCRUD Component

--- src/app/components/postCRUD/post-update/post-update.component.ts
+++ src/app/components/postCRUD/post-update/post-update.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder,FormControl,FormGroup } from '@angular/forms';
+import { PostingService } from 'src/app/services/api/posting.service';
+import { TagService } from 'src/app/services/api/tag.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-post-update',
   templateUrl: './post-update.component.html',
   styleUrls: ['./post-update.component.scss'],
 })
-export class PostUpdateComponent implements OnInit {
+export class PostUpdateComponent {
 
-  constructor() { }
+  //postForm: FormGroup;
 
-  ngOnInit() {}
+  post_text: string;
+  picture_list: string[] = [];
+  tags_list: any[] = [];
+
+  picture_url: string = '';
+
+  // knowTag: any[] = [];
+  tempTagSearch: any[] = [];
+  searchTagValue: string
+
+  constructor(
+    public formBulider: FormBuilder,
+    private router: Router,
+    private PostServ: PostingService,
+    private alertCtrl: AlertController,
+    private tagServ: TagService
+  ) { 
+  }
 
 }
