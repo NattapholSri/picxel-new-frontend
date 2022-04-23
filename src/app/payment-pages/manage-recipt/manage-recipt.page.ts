c620c48ac80692d2ca4bba84e455880970d92662
Nattaphol <s6101012630097@email.kmutnb.ac.th>
2022-04-23 15:30:19 +0700

    Update manage-recipt.page.ts

--- src/app/payment-pages/manage-recipt/manage-recipt.page.ts
+++ src/app/payment-pages/manage-recipt/manage-recipt.page.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+import { PaymentsService } from 'src/app/services/api/payments.service';
+import { UserService } from 'src/app/services/api/user.service';
+
+import { omiseRecipt } from 'src/app/services/data-model/omise.model';
+
 
 @Component({
   selector: 'app-manage-recipt',
@@ -7,9 +15,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageReciptPage implements OnInit {
 
-  constructor() { }
+  recipt_list:any[] = []
+
+  constructor(
+    private router:Router,
+    private ngZone:NgZone,
+    private paymentServ:PaymentsService,
+    private userServ: UserService,
+    private alertCtrl: AlertController
+  ) {
+    this.userServ.AutoLogout()
+
+    this.paymentServ.listCustomerCard().subscribe(
+      (res) => {
+        console.log(res.list.data)
+        //this.recipt_list = res.list.data
+      }
+    )
+  }
 
   ngOnInit() {
+  }
+
+  async attentionReciptAction(card_tk_id:string){
+    console.log(card_tk_id)
+    const alert = await this.alertCtrl.create({
+      header: 'ลบบัตรเครดิตนี้ ?',
+      buttons: [
+        {
+        text: 'ยกเลิก',
+        role: 'cancel'
+        },{
+          text: 'ลบ',
+          handler: () => {
+            this.deleteRecipt(card_tk_id)
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+  }
+
+  deleteRecipt(card_tk_id:string){
+    let data: omiseRecipt
+    this.paymentServ.updateRecipient(data).subscribe(
+      (res) => {
+        console.log(res)
+        location.reload()
+      },(err) => console.log(err)
+    )
+
   }
 
 }
